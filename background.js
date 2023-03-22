3f78c6a71ebe11c13ca9b535299b160300bc9c5b
Gabrijel Mrgan <ekvadior@gmail.com>
2023-03-22 19:22:59 +0100

    Update background.js

--- background.js
+++ background.js
@@ -2,6 +2,19 @@ function highlightTab(index) {
   chrome.tabs.highlight({'tabs': index}, function() {});
 }
 
+function toggleTabMute(tab) {
+
+  chrome.tabs.update(tab.id, {
+    muted: !tab.mutedInfo.muted
+  }, function() {})
+}
+
+function toggleYoutubePlayPause(tab) {
+  chrome.tabs.executeScript(tab.id, {
+    code: 'document.querySelector(".ytp-play-button").click()'
+  });
+}
+
 chrome.commands.onCommand.addListener(function(command) {
   chrome.tabs.query({}, function (all) {
     chrome.tabs.query({active: true}, function (tabs) {
@@ -10,8 +23,12 @@ chrome.commands.onCommand.addListener(function(command) {
 
         if (command === 'next-tab') {
           highlightTab((index + 1) % all.length);
-        } else if(command === 'prev-tab') {
+        } else if (command === 'prev-tab') {
           highlightTab((all.length + index - 1) % all.length);
+        } else if (command === 'mute-tab') {
+          toggleTabMute(tabs[0]);
+        } else if (command === 'play-pause') {
+          toggleYoutubePlayPause(tabs[0]);
         }
       }
     });
